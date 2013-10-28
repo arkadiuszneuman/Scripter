23342e4eab8fd99072791b304f37736249fc4bc0
Arkadiusz Neuman <mrmoro@o2.pl>
2013-10-28 19:43:35 +0100

    Klasa do obsługi configa

--- Skryper/Utilities/Cl_Config.cs
+++ Skryper/Utilities/Cl_Config.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Skryper.Utilities
 {
-    class Cl_Config
+    public class Cl_Config
     {
+        public readonly string ConfigPath = Application.ExecutablePath + "\\config.conf";
+
+        public void SaveConfig()
+        {
+            using (StreamWriter vrlStream = new StreamWriter(ConfigPath))
+            {
+                vrlStream.Write(Connection + ";" + Database);
+            }
+        }
+
+        public void LoadConfig()
+        {
+            if (File.Exists(ConfigPath))
+            {
+                using (StreamReader vrlStream = new StreamReader(ConfigPath))
+                {
+                    string vrlText = vrlStream.ReadLine();
+                    string[] vrlSplitted = vrlText.Split(';');
+
+                    if (vrlSplitted.Count() != 2)
+                    {
+                        throw new ArgumentException("Nieprawdłowy plik config.");
+                    }
+
+                    Connection = vrlSplitted[0];
+                    Database = vrlSplitted[1];
+                }
+            }
+        }
+
+        public string Connection { get; set; }
+        public string Database { get; set; }
     }
 }
